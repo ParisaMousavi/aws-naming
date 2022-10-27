5f0dfabe77dcf58ca3e1ed5ddbf916c6a3b997ac
Parisa Moosavinezhad <parisam@microsoft.com>
2022-10-27 14:28:48 +0200

    update

--- output.tf
+++ output.tf
@@ -1,23 +1,23 @@
 output "alb_name" {
-  value = format("${var.prefix}-alb-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-alb-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
 
 output "s3_name" {
-  value = format("${var.prefix}-s3-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-s3-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
 
 output "vpc_name" {
-  value = format("${var.prefix}-vpc-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-vpc-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
 
 output "snet_name" {
-  value = format("${var.prefix}-snet-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-snet-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
 
 output "ec2_name" {
-  value = format("${var.prefix}-ec2-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-ec2-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
 
 output "eks_name" {
-  value = format("${var.prefix}-eks-%s", join("-", compact([var.name, var.environment, var.region_shortname])))
+  value = format("${var.prefix}-eks-%s", join("-", compact([lower(var.name), lower(var.environment), lower(var.region_shortname)])))
 }
